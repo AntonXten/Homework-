64021901f60f22cc3b2453203928bb84051f3d25
Anton <antonxten@gmail.com>
2022-04-13 21:25:50 +0300

    Решаю задачки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+/*
+int numA = 5;
+int numB = 7;
+
+if (numA>numB)
+{
+    Console.WriteLine(numA);
+    Console.WriteLine(numB);
+}
+else if (numB>numA);
+{
+    Console.WriteLine(numB);
+    Console.WriteLine(numA);
+}
+*/
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+
+int numA = 2;
+int numB = 3;
+int numC = 7;
+
+if(numA>numB)
+{
+    Console.WriteLine(numA);
+}
+else if(numB>numC)
+{
+    Console.WriteLine(numB);
+}
+else if(numC>numA)
+{
+    Console.WriteLine(numC);
+} 
+
+*/
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+
+int i;
+Console.Write("Введите число : ");  
+i = Convert.ToInt32(Console.ReadLine());
+
+if (i % 2 == 0)
+{
+    Console.Write("Введенное число является четным");
+}
+else
+{
+     Console.Write("Введенное число нечетное");
+} 
+
+*/
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 1 * num;
+
+
+        
+
+        
+           
