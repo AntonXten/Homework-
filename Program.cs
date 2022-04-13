e4941f17df369c2f0f480efcfecdfae921203951
Anton <antonxten@gmail.com>
2022-04-13 21:50:42 +0300

    Пытаюсь решить

--- Program.cs
+++ Program.cs
@@ -58,12 +58,6 @@ else
 
 */
 
-//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
-Console.Write("Input integer number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int current = 1 * num;
-
 
         
 
